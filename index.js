36573aabefc7d75a063ba9afeb7c41ae297d17f6
Juan Salazar <ferthelet@gmail.com>
2023-03-05 08:17:45 -0600

    adds intro with language

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ function introduction(name) {
     return `Hi, my name is ${name}.`
 }
 
-
+function introductionWithLanguage(name, language) {
+    //takes in two arguments, a name and a language, and returns a phrase using those arguments
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
