6a76d25d302a6113c7a7b90809dc14747d58feda
Juan Salazar <ferthelet@gmail.com>
2023-03-05 08:23:40 -0600

    adds intro with optional language

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function introductionWithLanguage(name, language) {
     //takes in two arguments, a name and a language, and returns a phrase using those arguments
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    //takes in two arguments, a name and a language, and language defaults to JavaScript
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
